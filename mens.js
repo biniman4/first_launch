ebf8fe5fa5a169ef69feef3dc0bd209d82c15e39
codewithdann0 <codewithdann028@gmail.com>
2024-06-03 05:16:11 -0700

    The Enire page style which is reponsive

---
+++ mens.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("menu-icon");
+    const navBar = document.querySelector(".nav-bar");
+    const menuItems = document.querySelector(".nav-bar ul");
+
+    menuIcon.addEventListener("click", function() {
+        navBar.classList.toggle("expanded");
+        menuItems.classList.toggle("hidden");
+    });
+});
+document.getElementById('cloth1').style.backgroundImage = "url('./mens-Img/cloth1.jpg')";
+document.getElementById('cloth2').style.backgroundImage = "url('./mens-Img/cloth2.jpg')";
+document.getElementById('cloth3').style.backgroundImage = "url('./mens-Img/cloth3.jpg')";
+document.getElementById('cloth4').style.backgroundImage = "url('./mens-Img/cloth4.jpg')";
+document.getElementById('cloth5').style.backgroundImage = "url('./mens-Img/cloth5.jpg')";
+document.getElementById('cloth6').style.backgroundImage = "url('./mens-Img/cloth6.jpg')";
+document.getElementById('cloth7').style.backgroundImage = "url('./mens-Img/cloth7.jpg')";
+document.getElementById('cloth8').style.backgroundImage = "url('./mens-Img/cloth8.jpg')";
+document.getElementById('cloth9').style.backgroundImage = "url('./mens-Img/cloth9.jpg')";
+document.getElementById('cloth10').style.backgroundImage = "url('./mens-Img/cloth10.jpg')";
+document.getElementById('cloth11').style.backgroundImage = "url('./mens-Img/cloth11.jpg')";
+document.getElementById('cloth12').style.backgroundImage = "url('./mens-Img/cloth12.jpg')";
