d2cf3e7ef43952e1b093d44ef7b0d50e8637552c
codewithdann0 <codewithdann028@gmail.com>
2024-06-14 01:56:26 -0700

    The nav bar made responsive the javascript code more readable

--- mens.js
+++ mens.js
@@ -1,22 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.getElementById("menu-icon");
-    const navBar = document.querySelector(".nav-bar");
-    const menuItems = document.querySelector(".nav-bar ul");
+// Array of image URLs
+const imageUrls = [
+    './mens-Img/cloth1.jpg',
+    './mens-Img/cloth2.jpg',
+    './mens-Img/cloth3.jpg',
+    './mens-Img/cloth4.jpg',
+    './mens-Img/cloth5.jpg',
+    './mens-Img/cloth6.jpg',
+    './mens-Img/cloth7.jpg',
+    './mens-Img/cloth8.jpg',
+    './mens-Img/cloth9.jpg',
+    './mens-Img/cloth10.jpg',
+    './mens-Img/cloth11.jpg',
+    './mens-Img/cloth12.jpg'
+];
 
-    menuIcon.addEventListener("click", function() {
-        navBar.classList.toggle("expanded");
-        menuItems.classList.toggle("hidden");
-    });
-});
-document.getElementById('cloth1').style.backgroundImage = "url('./mens-Img/cloth1.jpg')";
-document.getElementById('cloth2').style.backgroundImage = "url('./mens-Img/cloth2.jpg')";
-document.getElementById('cloth3').style.backgroundImage = "url('./mens-Img/cloth3.jpg')";
-document.getElementById('cloth4').style.backgroundImage = "url('./mens-Img/cloth4.jpg')";
-document.getElementById('cloth5').style.backgroundImage = "url('./mens-Img/cloth5.jpg')";
-document.getElementById('cloth6').style.backgroundImage = "url('./mens-Img/cloth6.jpg')";
-document.getElementById('cloth7').style.backgroundImage = "url('./mens-Img/cloth7.jpg')";
-document.getElementById('cloth8').style.backgroundImage = "url('./mens-Img/cloth8.jpg')";
-document.getElementById('cloth9').style.backgroundImage = "url('./mens-Img/cloth9.jpg')";
-document.getElementById('cloth10').style.backgroundImage = "url('./mens-Img/cloth10.jpg')";
-document.getElementById('cloth11').style.backgroundImage = "url('./mens-Img/cloth11.jpg')";
-document.getElementById('cloth12').style.backgroundImage = "url('./mens-Img/cloth12.jpg')";
+// Loop through elements and set background images
+for (let i = 0; i < imageUrls.length; i++) {
+    let clothId = `cloth${i + 1}`;
+    let clothElement = document.getElementById(clothId);
+    if (clothElement) {
+        clothElement.style.backgroundImage = `url('${imageUrls[i]}')`;
+    }
+}
+
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-bar ul');
+    navLinks.classList.toggle('show');
+}
